28ba13fa75f24625d72185a4dd0d1497cc4124c5
Zegos73 <65068135+Zegos73@users.noreply.github.com>
2023-05-19 16:38:58 +0400

    Add files via upload

--- 6 лаба усл.py
+++ 6 лаба усл.py
@@ -1,3 +1,4 @@
+# На плоскости задано К точек. Сформировать все возможные варианты выбора множества точек из них на проверку того, что они принадлежат одной прямой.
 points = [(1, 1), (3, 3), (5, 5), (8, 8), (9, 9), (12,12),(101,101)]
 
 def are_points_on_line(points):
@@ -21,13 +22,13 @@ def count_odd_points_on_line(line_points):
     odd_points = [point for point in line_points if point[0] % 2 != 0 and point[1] % 2 != 0]
     return len(odd_points)
 
-max_points_on_line = 0  # инициализируем максимальное количество точек на прямой
-max_line_points = []  # инициализируем список точек на прямой с максимальным количеством точек
+max_points_on_line = 0  
+max_line_points = []  
 
 for i in range(2, len(points) + 1):
     for comb in generate_combinations(points, i):
         line_points = list(comb)
-        if all(point[0] % 2 != 0 and point[1] % 2 != 0 for point in line_points):  # ограничение на нечетные координаты
+        if all(point[0] % 2 != 0 and point[1] % 2 != 0 for point in line_points):
             if are_points_on_line(line_points):
                 odd_points_count = count_odd_points_on_line(line_points)
                 if odd_points_count > max_points_on_line:
