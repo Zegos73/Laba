06271075d9abb5f7d76bc89b020b99d71037c42a
Zegos73 <65068135+Zegos73@users.noreply.github.com>
2024-01-30 09:32:07 +0400

    Add files via upload

--- 12 laba12.py
+++ 12 laba12.py
@@ -1,45 +1,37 @@
-#|х*n!|/n!
-#У алгоритма д.б. линейная сложность.
-#Знак первого слагаемого  -.
+#Вычислить сумму знакопеременного ряда |х**2n|/(2n)!, где х-матрица ранга к (к и матрица задаются случайным образом), n - номер слагаемого.
+#Сумма считается вычисленной, если точность вычислений будет не меньше t знаков после запятой.
+#У алгоритма д.б. линейная сложность. Знак первого слагаемого  -.
 import numpy as np
-import math
 import random
-# Количество знаков после запятой
+#Создание матрицы и ввод данных
 while True:
-    t = input("Введите количество знаков после запятой: ")
+    t = input("Введите количество знаков после запятой при вычислении неточности : ")
     if t.isdigit():
         if int(t) == 0:
-            print("Введите другое число")
+            print("Вы ввели ноль. Введите другое число")
         else:
             t = int(t)
             break
     else:
-        print("Вы ввести натуральное число")
-# Генерация матрицы случайным образом 
+        print("Вы должны были ввести натуральное число")
 k = random.randint(1,4)
-x = np.random.uniform(-1, 1, size=(k, k))
+x = np.random.uniform(-1, 1, (k, k))
 print(f"Матрица: \n{x}")
 #Определитель
 print(f"Определитель: {np.linalg.det(x)}")
 #вычисление суммы знакопеременного ряда
 n = 1
-result = 0
-sign = -1
-before_digit = 0
-accuracy = 1
-fact_denominator = 1
-fact_numerator = 1
+before_digit, result, accuracy = 0, 0, 1
+fact_denominator, fact_numerator,sign = 1, 1,-1
 
 while abs(accuracy) > (0.1 ** t):
     before_digit += result
-    fact_numerator *= (n - 1) * (n)
-    fact_denominator *= (n - 1) * (n)
-    result += sign * np.linalg.det(x * fact_numerator) / fact_denominator
+    numerator = (2 * n) 
+    fact_denominator *= np.prod(2 * n) * (2 * n + 1)
+    result += sign * np.linalg.det(x ** fact_numerator) / fact_denominator
     n += 1
     accuracy = abs(before_digit - result)
     before_digit = 0
     sign *= -1
-
-# Вывод результатов
-print(f"Итоговая сумма ряда: {result:.{t}f}")
-print(f"Количество итераций: {n-1}")
+print(f"\nИтоговая сумма: {result:.{t}f}")
+print(f"Кол-во итераций: {n-1}")
